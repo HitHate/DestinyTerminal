fa9e59db7850d1f5d06d8aa79fc140cb598813c8
HitHate <984208347@qq.com>
2022-09-22 21:49:09 +0800

    百度地图定位demo 2.0

--- app/src/main/java/com/example/destinyterminal/MainActivity.java
+++ app/src/main/java/com/example/destinyterminal/MainActivity.java
@@ -16,9 +16,11 @@ import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 
 
@@ -62,23 +64,30 @@ public class MainActivity extends AppCompatActivity {
         mLocationClient.registerLocationListener(new MyLocationListener());
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-
+        // 定位初始化
+        mLocationClient = new LocationClient(this);
+        MyLocationListener myListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myListener);
+        LocationClientOption option = new LocationClientOption();
+        // 打开gps
+        option.setOpenGps(true);
+        // 设置坐标类型
+        option.setCoorType("bd09ll");
+        option.setScanSpan(1000);
         mapView = findViewById(R.id.bmapView);
         baiduMap = mapView.getMap();
         tv_Lat = findViewById(R.id.tv_Lat);
         tv_Lon = findViewById(R.id.tv_Lon);
         tv_Add = findViewById(R.id.tv_Add);
-        LocationClientOption option = new LocationClientOption();
         //设置扫描时间间隔
         option.setScanSpan(1000);
         //设置定位模式，三选一
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
-        /*option.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);
-        option.setLocationMode(LocationClientOption.LocationMode.Device_Sensors);*/
         //设置需要地址信息
         option.setIsNeedAddress(true);
         //保存定位参数
         mLocationClient.setLocOption(option);
+        baiduMap.setMyLocationEnabled(true);
     }
     //内部类，百度位置监听器
     private class MyLocationListener  implements BDLocationListener {
@@ -92,6 +101,24 @@ public class MainActivity extends AppCompatActivity {
                     bdLocation.getLocType()==BDLocation.TypeNetWorkLocation){
                 navigateTo(bdLocation);
             }
+            if (bdLocation == null || mapView == null) {
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(bdLocation.getRadius())// 设置定位数据的精度信息，单位：米
+                    .direction(bdLocation.getDirection()) // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .latitude(bdLocation.getLatitude())
+                    .longitude(bdLocation.getLongitude())
+                    .build();
+            // 设置定位数据, 只有先允许定位图层后设置数据才会生效
+            baiduMap.setMyLocationData(locData);
+            if (isFirstLocate) {
+                isFirstLocate = false;
+                LatLng latLng = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(latLng).zoom(20.0f);
+                baiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
         }
     }
     private void navigateTo(BDLocation bdLocation) {
@@ -102,6 +129,7 @@ public class MainActivity extends AppCompatActivity {
             isFirstLocate = false;
         }
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -116,6 +144,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mLocationClient.stop();
+        baiduMap.setMyLocationEnabled(false);
         mapView.onDestroy();
     }
 }
